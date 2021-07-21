d85c92cd9e4d23bf73c5015fef1a10bebc80be91
Robbendebiene <robbendebiene@mailbox.org>
2021-07-21 12:07:29 +0200

    outsourced home screen controls

---
+++ lib/widgets/home-controlls.dart
@@ -0,0 +1,92 @@
+
+import 'package:flutter/material.dart';
+
+/**
+ * Builds the action buttons which overlay the map.
+ */
+class HomeControlls extends StatelessWidget {
+  final double buttonSpacing;
+  final double buttonIconSize;
+  final ButtonStyle buttonStyle;
+
+  final void Function() zoomIn;
+  final void Function() zoomOut;
+  final void Function() moveToUserLocation;
+
+
+  const HomeControlls({
+    Key? key,
+    required this.buttonStyle,
+    required this.zoomIn,
+    required this.zoomOut,
+    required this.moveToUserLocation,
+    this.buttonSpacing = 10.0,
+    this.buttonIconSize = 25.0
+  }) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(
+        0,
+        buttonSpacing + MediaQuery.of(context).padding.top,
+        0,
+        buttonSpacing + MediaQuery.of(context).padding.bottom,
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          ElevatedButton(
+            style: buttonStyle,
+            child: Icon(
+              Icons.menu,
+              size: buttonIconSize,
+              color: Colors.black,
+            ),
+            onPressed: Scaffold.of(context).openDrawer,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              ElevatedButton(
+                style: buttonStyle,
+                child: Icon(
+                  Icons.my_location,
+                  size: buttonIconSize,
+                  color: Colors.black,
+                ),
+                onPressed: moveToUserLocation,
+              ),
+              SizedBox (
+                height: buttonSpacing
+              ),
+              ElevatedButton(
+                style: buttonStyle,
+                child: Icon(
+                  Icons.add,
+                  size: buttonIconSize,
+                  color: Colors.black,
+                ),
+                onPressed: zoomIn,
+              ),
+              SizedBox (
+                height: buttonSpacing
+              ),
+              ElevatedButton(
+                style: buttonStyle,
+                child: Icon(
+                  Icons.remove,
+                  size: buttonIconSize,
+                  color: Colors.black,
+                ),
+                onPressed: zoomOut,
+              ),
+            ]
+          )
+        ],
+      )
+    );
+  }
+}
