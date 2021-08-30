17e9a4bd063baa245a536bde5bae64d06b14cba5
René Apitzsch <rene.apitzsch@etit.tu-chemnitz.de>
2021-08-30 11:04:06 +0200

    Remove cupertino.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:math';
 import 'dart:typed_data';
 import 'dart:ui';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 import 'package:flutter/services.dart';
