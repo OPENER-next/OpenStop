70c5ce5f60dfc1b1b7a90c06c866bc13b8d1d41d
Robbendebiene <robbendebiene@mailbox.org>
2021-09-01 10:38:09 +0200

    Remove unnecessary dhid property

--- lib/models/stop.dart
+++ lib/models/stop.dart
@@ -4,14 +4,11 @@ import 'package:mapbox_gl/mapbox_gl.dart';
 /// A basic representation of a public transport stop/halt.
 
 class Stop {
-  final String dhid;
-
   final String name;
 
   final LatLng location;
 
   Stop({
-    required this.dhid,
     required this.name,
     required this.location
   });
@@ -21,7 +18,6 @@ class Stop {
 
   factory Stop.fromOverpassJSON(Map<String, dynamic> element) {
     return Stop(
-      dhid: "",
       name: element['tags']?['name'] ?? 'Unknown Name',
       location: LatLng(element['center']['lat'], element['center']['lon'])
     );
@@ -29,12 +25,11 @@ class Stop {
 
 
   @override
-  String toString() => '$runtimeType - dhid: $dhid; user: $name; text: $name; location: $location';
+  String toString() => '$runtimeType - user: $name; text: $name; location: $location';
 
 
   @override
   int get hashCode =>
-    dhid.hashCode ^
     name.hashCode ^
     location.hashCode;
 
@@ -44,7 +39,6 @@ class Stop {
     identical(this, o) ||
     o is Stop &&
     runtimeType == o.runtimeType &&
-    dhid == o.dhid &&
     name == o.name &&
     location == o.location;
 }
