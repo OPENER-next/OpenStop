e456cdfb2a272946d9a8be3783baad14a3f29707
Robbendebiene <robbendebiene@mailbox.org>
2021-09-23 13:56:28 +0200

    Add basic user location indicator
    This commit also reduces the widget hierarchy, improves the performance and fixes the RangeError. For some unknown reason the heading of the user doesn't work well yet.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 import '/commons/stream_debouncer.dart';
 import '/widgets/stop_area_indicator.dart';
@@ -26,9 +27,15 @@ class HomeScreen extends StatefulWidget {
 
 
 class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  final Stream<Position> _locationStream = Geolocator.getPositionStream(
+    intervalDuration: Duration(seconds: 1)
+  );
+
   final MapController _mapController = MapController();
 
   final _stopQueryHandler = StopQueryHandler();
+
+  static const degrees2Radians = pi/180;
 
   static const double _initialSheetSize = 0.4;
 
@@ -80,97 +87,149 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
       resizeToAvoidBottomInset: false,
       drawer: HomeSidebar(),
       // use builder to get scaffold context
-      body: Builder(builder: (context) => Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          StreamBuilder<Iterable<Stop>>(
-            stream: _stopQueryHandler.stops,
-            initialData: [],
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                for (var stop in snapshot.data!) {
-                  _markers.add(Marker(
-                    point: stop.location,
-                    builder: (context) => GestureDetector(
-                      behavior: HitTestBehavior.translucent,
-                      onTap: () => _onStopAreaTap(stop),
-                      child: StopAreaIndicator()
-                    )
-                  ));
-                }
+      body: Builder(builder: (context) =>
+        // TODO: tracking + clustering
+        FlutterMap(
+          mapController: _mapController,
+          options: MapOptions(
+            onTap: (position, location) => _selectedQuestion.value = null,
+            enableMultiFingerGestureRace: true,
+            center: LatLng(50.8261, 12.9278),
+            zoom: 15.0,
+            plugins: [
+              MarkerClusterPlugin(),
+            ],
+          ),
+          children: [
+            TileLayerWidget(
+              options: TileLayerOptions(
+                urlTemplate: "https://osm-2.nearest.place/retina/{z}/{x}/{y}.png",
+              ),
+            ),
+            // place circle layer before marker layer due to: https://github.com/fleaflet/flutter_map/issues/891
+            StreamBuilder<Position>(
+              stream: _locationStream,
+              builder: (context, snapshot) {
+                return GroupLayerWidget(
+                  options: GroupLayerOptions(
+                    group: (!snapshot.hasData) ? const [] : [
+                      // display location accuracy circle
+                      if (snapshot.data!.accuracy > 0) CircleLayerOptions(
+                        circles: [
+                          CircleMarker(
+                            color: Colors.blue.withOpacity(0.3),
+                            useRadiusInMeter: true,
+                            point: LatLng(snapshot.data!.latitude, snapshot.data!.longitude),
+                            radius: snapshot.data!.accuracy
+                          )
+                        ]
+                      ),
+                      // display location marker
+                      MarkerLayerOptions(
+                        markers: [
+                          Marker(
+                            width: 20,
+                            height: 20,
+                            point: LatLng(snapshot.data!.latitude, snapshot.data!.longitude),
+                            builder: (context) => AnimatedContainer(
+                              transform: Matrix4.rotationZ(snapshot.data!.heading * degrees2Radians),
+                              transformAlignment: Alignment.center,
+                              padding: EdgeInsets.all(2),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.white,
+                                  width: 3,
+                                ),
+                                shape: BoxShape.circle,
+                                color: Colors.blue,
+                              ),
+                              curve: Curves.ease,
+                              duration: Duration(milliseconds: 300),
+                              child: const FittedBox(
+                                child: const Icon(
+                                  Icons.navigation_sharp,
+                                  color: Colors.white
+                                )
+                              ),
+                            )
+                          )
+                        ]
+                      ),
+                    ]
+                  )
+                );
               }
-
-              // TODO: user location + tracking + clustering
-              // TODO: fix  RangeError
-              return FlutterMap(
-                mapController: _mapController,
-                options: MapOptions(
-                  onTap: (position, location) => _selectedQuestion.value = null,
-                  enableMultiFingerGestureRace: true,
-                  center: LatLng(50.8261, 12.9278),
-                  zoom: 15.0,
-                  plugins: [
-                    MarkerClusterPlugin(),
-                  ],
-                ),
-                layers: [
-                  TileLayerOptions(
-                    urlTemplate: "https://osm-2.nearest.place/retina/{z}/{x}/{y}.png",
-                    attributionAlignment: Alignment.bottomLeft,
-                    attributionBuilder: (context) {
-                      return Padding(
-                        padding: EdgeInsets.only(left: 10, bottom: MediaQuery.of(context).padding.bottom + 10),
-                        child: Text("© OpenStreetMap contributors")
-                      );
-                    },
-                  ),
-                  MarkerLayerOptions(
+            ),
+            StreamBuilder<Iterable<Stop>>(
+              stream: _stopQueryHandler.stops,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  for (var stop in snapshot.data!) {
+                    _markers.add(Marker(
+                      point: stop.location,
+                      builder: (context) => GestureDetector(
+                        behavior: HitTestBehavior.translucent,
+                        onTap: () => _onStopAreaTap(stop),
+                        child: StopAreaIndicator()
+                      )
+                    ));
+                  }
+                }
+                return MarkerLayerWidget(
+                  options: MarkerLayerOptions(
                     markers: _markers
-                  )
-                ],
-              );
-            }
-          ),
-          FutureBuilder(
-            future: _mapController.onReady,
-            builder: (BuildContext context, AsyncSnapshot<Null> snapshot) {
-              // only show controls when map creation finished
-              return AnimatedSwitcher(
-                duration: Duration(milliseconds: 1000),
-                child: snapshot.connectionState == ConnectionState.done
-                  ? HomeControls(
-                    mapController: _mapController
-                  )
-                  : Container(
-                    color: Colors.white
-                  )
-              );
-            }
-          ),
-          Positioned(
-            top: MediaQuery.of(context).padding.top + 15,
-            right: 0.0,
-            left: 0.0,
-            child: ValueListenableBuilder<int>(
-              builder: (BuildContext context, int value, Widget? child) =>
-                AnimatedSwitcher(
-                  switchInCurve: Curves.elasticOut,
-                  switchOutCurve: Curves.elasticOut,
-                  transitionBuilder: (Widget child, Animation<double> animation) =>
-                    ScaleTransition(child: child, scale: animation),
-                  duration: Duration(milliseconds: 500),
-                  child: value > 0 ? child : const SizedBox.shrink()
-                ),
-              valueListenable: _stopQueryHandler.pendingQueryCount,
-              child: LoadingIndicator()
-            )
-          ),
-          QuestionSheet(
-            question: _selectedQuestion,
-            initialSheetSize: _initialSheetSize
-          ),
-        ]
-      )),
+                  ),
+                );
+              }
+            ),
+          ],
+          nonRotatedChildren: [
+            Positioned(
+              left: 10,
+              bottom: MediaQuery.of(context).padding.bottom + 10,
+              child: Text("© OpenStreetMap contributors")
+            ),
+            FutureBuilder(
+              future: _mapController.onReady,
+              builder: (BuildContext context, AsyncSnapshot<Null> snapshot) {
+                // only show controls when map creation finished
+                return AnimatedSwitcher(
+                  duration: Duration(milliseconds: 1000),
+                  child: snapshot.connectionState == ConnectionState.done
+                    ? HomeControls(
+                      mapController: _mapController
+                    )
+                    : Container(
+                      color: Colors.white
+                    )
+                );
+              }
+            ),
+            Positioned(
+              top: MediaQuery.of(context).padding.top + 15,
+              right: 0.0,
+              left: 0.0,
+              child: ValueListenableBuilder<int>(
+                builder: (BuildContext context, int value, Widget? child) =>
+                  AnimatedSwitcher(
+                    switchInCurve: Curves.elasticOut,
+                    switchOutCurve: Curves.elasticOut,
+                    transitionBuilder: (Widget child, Animation<double> animation) =>
+                      ScaleTransition(child: child, scale: animation),
+                    duration: Duration(milliseconds: 500),
+                    child: value > 0 ? child : const SizedBox.shrink()
+                  ),
+                valueListenable: _stopQueryHandler.pendingQueryCount,
+                child: LoadingIndicator()
+              )
+            ),
+            QuestionSheet(
+              question: _selectedQuestion,
+              initialSheetSize: _initialSheetSize
+            ),
+          ],
+        ),
+      ),
     );
   }
 
