7f2cd397e9508a6260483aefb347db49d83d7c38
René Apitzsch <rene.apitzsch@etit.tu-chemnitz.de>
2021-12-18 01:34:17 +0100

    Update About page
    Add terms_of_use and privacy_policy
    Add version number via additional package
    Fix button padding to completely see funding logos
    Fix funding logos in landscape mode on larger screens

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -1,16 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
-class About extends StatelessWidget {
+// Screens
+import 'terms_of_use.dart';
+import 'privacy_policy.dart';
+
+class About extends StatefulWidget {
   const About({Key? key}) : super(key: key);
 
+  @override
+  State<About> createState() => _AboutState();
+}
+
+class _AboutState extends State<About> {
   final String _urlContributors = 'https://github.com/OPENER-next/OPENER-next/graphs/contributors';
   final String _urlCode = 'https://github.com/OPENER-next';
   final String _urlLicence = 'https://github.com/OPENER-next/OPENER-next/blob/master/LICENSE';
   final String _urlIdea = 'https://www.tu-chemnitz.de/etit/sse';
 
+  late PackageInfo _packageInfo = PackageInfo(
+    appName: 'Unknown',
+    packageName: 'Unknown',
+    version: 'Unknown',
+    buildNumber: 'Unknown',
+  );
+
+  Future<void> _initPackageInfo() async {
+    final info = await PackageInfo.fromPlatform();
+    setState(() {
+      _packageInfo = info;
+    });
+  }
+
   void _launchUrl(_url) async {
-    if (!await launch(_url)) throw 'Could not launch $_url';
+    if (!await launch(_url)) throw '$_url kann nicht aufgerufen werden';
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initPackageInfo();
   }
 
   @override
@@ -20,10 +50,11 @@ class About extends StatelessWidget {
           title: const Text('Über OPENER next'),
           backgroundColor: Colors.white10,
           elevation: 0.0,
-        ),
+          ),
         body: Scrollbar(
           child: SingleChildScrollView(
             scrollDirection: Axis.vertical,
+            padding: EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom),
             child: Column(
               children: [
                 Padding(
@@ -46,7 +77,7 @@ class About extends StatelessWidget {
                       color: Colors.black54
                   ),
                   title: const Text('Version'),
-                  subtitle: const Text('1.12.3'),
+                  subtitle: Text('${_packageInfo.version}+${_packageInfo.buildNumber}'),
                   onTap: () {},
                 ),
                 ListTile(
@@ -58,6 +89,18 @@ class About extends StatelessWidget {
                   title: const Text('Autoren'),
                   subtitle: const Text('OPENER next Contributors'),
                   onTap: () => _launchUrl(_urlContributors),
+                ),
+                ListTile(
+                  dense: true,
+                  isThreeLine: true,
+                  leading: const Icon(
+                      Icons.lightbulb,
+                      color: Colors.black54
+                  ),
+                  title: const Text('Idee'),
+                  subtitle: const Text('Technische Universität Chemnitz\n'
+                      'Professur Schaltkreis- und Systementwurf'),
+                  onTap: () => _launchUrl(_urlIdea),
                 ),
                 ListTile(
                   dense: true,
@@ -81,31 +124,55 @@ class About extends StatelessWidget {
                 ),
                 ListTile(
                   dense: true,
-                  isThreeLine: true,
                   leading: const Icon(
-                      Icons.lightbulb,
+                      Icons.privacy_tip,
                       color: Colors.black54
                   ),
-                  title: const Text('Idee'),
-                  subtitle: const Text('Technische Universität Chemnitz\n'
-                          'Professur Schaltkreis- und Systementwurf'),
-                  onTap: () => _launchUrl(_urlIdea),
+                  title: const Text('Datenschutzerklärung'),
+                  onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const PrivacyPolicy(),
+                      )
+                  ),
                 ),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Flexible(
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                          child: Image.asset('assets/logos/BMVI_Fz_2017_Office_Farbe_de.png'),
-                        )
-                    ),
-                    Expanded(
-                        child: Padding(
-                          padding:const EdgeInsets.symmetric(horizontal: 20.0),
-                          child: Image.asset('assets/logos/mFUND_Logo_sRGB.png'),
-                        )),
-                  ],
+                ListTile(
+                  dense: true,
+                  leading: const Icon(
+                      Icons.contact_page,
+                      color: Colors.black54
+                  ),
+                  title: const Text('Nutzungsbedingungen'),
+                  onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const TermsOfUse(),
+                      )
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  height: 160,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Flexible(
+                        flex: 1,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                            child: Image.asset(
+                                'assets/logos/BMVI_Fz_2017_Office_Farbe_de.png'),
+                          )
+                      ),
+                      Flexible(
+                        flex: 1,
+                          child: Padding(
+                            padding:const EdgeInsets.fromLTRB(20.0,50.0,20.0,50.0),
+                            child: Image.asset(
+                                'assets/logos/mFUND_Logo_sRGB.png'),
+                          )
+                      )
+                    ],
+                  ),
                 )
               ],
             ),
