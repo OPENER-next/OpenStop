83bb1787f7eeb3845715d081541ca9bf704cefb8
Robbendebiene <robbendebiene@mailbox.org>
2022-01-04 09:53:26 +0100

    Rework stop area to represent a circle

--- lib/models/stop_area.dart
+++ lib/models/stop_area.dart
@@ -6,36 +6,47 @@ import '/models/stop.dart';
 
 /// A bundle of multiple nearby [Stop]s.
 
-class StopArea {
+class StopArea extends Circle {
   static const _distance = Distance();
 
   final Set<Stop> stops;
 
-  late final LatLngBounds bounds;
-
-  late final double diameter;
-
   StopArea(
-    Iterable<Stop> stops
-  ) : stops = Set.unmodifiable(stops) {
-    bounds = _calculateBounds();
-    diameter = _calculateDiameter();
+    Iterable<Stop> stops,
+    LatLng center,
+    double radius,
+  ) : stops = Set.unmodifiable(stops), super(center, radius);
+
+  double get diameter => radius + radius;
+
+  /// Returns the bounding box enclosing the stop area circle.
+
+  LatLngBounds get bounds {
+    final corner1 = _distance.offset(
+      _distance.offset(center, radius, 0),
+      radius,
+      270
+    );
+    final corner2 = _distance.offset(
+      _distance.offset(center, radius, 180),
+      radius,
+      90
+    );
+
+    return LatLngBounds(corner1, corner2);
   }
 
-  LatLng get center => bounds.center;
+  /// Returns the bounding box enclosing all stops treated as points.
 
-  LatLngBounds _calculateBounds() {
+  LatLngBounds get stopBounds {
     return LatLngBounds.fromPoints(
       stops.map<LatLng>((stop) => stop.location).toList(growable: false)
     );
   }
 
-  double _calculateDiameter() {
-    return _distance(bounds.northWest, bounds.southEast);
-  }
-
   @override
-  String toString() => 'StopArea(stops: $stops, bounds: $bounds, diameter: $diameter)';
+  String toString() => 'StopArea(stops: $stops, center: $center, radius: $radius)';
+
 
   @override
   bool operator ==(Object other) {
@@ -44,10 +55,10 @@ class StopArea {
 
     return other is StopArea &&
       setEquals(other.stops, stops) &&
-      other.bounds == bounds &&
-      other.diameter == diameter;
+      other.center == center &&
+      other.radius == radius;
   }
 
   @override
-  int get hashCode => stops.hashCode ^ bounds.hashCode ^ diameter.hashCode;
+  int get hashCode => stops.hashCode ^ center.hashCode ^ radius.hashCode;
 }
