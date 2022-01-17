513f790c34ae47b6781b3145539f4b9968b5426f
René Apitzsch <rene.apitzsch@etit.tu-chemnitz.de>
2022-01-17 12:08:38 +0100

    Fix unimportant error in widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,16 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:opener_next/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp(
+        sharedPreferences: await SharedPreferences.getInstance()
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
