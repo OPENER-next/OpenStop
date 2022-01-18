471ba140952fc934d95d410918ccbcc976e6d865
Robbendebiene <robbendebiene@mailbox.org>
2022-01-18 10:01:29 +0100

    Adjust settings page style

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -22,63 +22,72 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeMode = context.select<PreferencesProvider, ThemeMode>((preferences) => preferences.themeMode);
+    final difficulty = context.select<PreferencesProvider, DifficultyLevel>((preferences) => preferences.difficulty);
+
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Einstellungen'),
-        ),
-        body: Scrollbar(
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.vertical,
-                  padding: EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom),
-                  child: Column(
-                    children: [
-                      ListTile(
-                        leading: const Icon(Icons.palette),
-                        title: const Text('Design'),
-                        trailing: Text(themeModesMap[context.select<PreferencesProvider, ThemeMode>((preferences) => preferences.themeMode)] ?? 'Unbekannt'),
-                        //subtitle: const Text('Farbliche Darstellung'),
-                        onTap: () async {
-                          final selection = await showDialog<ThemeMode>(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return SelectDialog(
-                                  valueLabelMap: themeModesMap,
-                                  value: context.select<PreferencesProvider, ThemeMode>((preferences) => preferences.themeMode),
-                                  title: const Text('Design'),
-                                );
-                              }
-                          );
-                          if (selection != null) {
-                            context.read<PreferencesProvider>().themeMode = selection;
-                          }
-                        },
-                      ),
-                      const Divider(height: 1),
-                      ListTile(
-                        leading: const Icon(Icons.line_weight),
-                        title: const Text('Schwierigkeitsgrad'),
-                        trailing: Text(difficultyMap[context.select<PreferencesProvider, DifficultyLevel>((preferences) => preferences.difficulty)] ?? 'Unbekannt'),
-                        //subtitle: const Text('Schwere der Fragen'),
-                        onTap: () async {
-                          final selection = await showDialog<DifficultyLevel>(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return SelectDialog(
-                                  valueLabelMap: difficultyMap,
-                                  value: context.select<PreferencesProvider, DifficultyLevel>((preferences) => preferences.difficulty),
-                                  title: const Text('Schwierigkeitsgrad'),
-                                );
-                              }
-                          );
-                          if (selection != null) {
-                            context.read<PreferencesProvider>().difficulty = selection;
-                          }
-                        },
-                      )
-                    ],
-                  ),
+      appBar: AppBar(
+        title: const Text('Einstellungen'),
+      ),
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          padding: EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom),
+          child: Column(
+            children: [
+              ListTile(
+                // used to vertically center the icon
+                leading: const SizedBox(
+                  height: double.infinity,
+                  child: Icon(Icons.palette),
                 ),
+                title: const Text('Farbliche Darstellung der App'),
+                subtitle: Text(themeModesMap[themeMode] ?? 'Unbekannt'),
+                onTap: () async {
+                  final selection = await showDialog<ThemeMode>(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return SelectDialog(
+                        valueLabelMap: themeModesMap,
+                        value: context.select<PreferencesProvider, ThemeMode>((preferences) => preferences.themeMode),
+                        title: const Text('Design'),
+                      );
+                    }
+                  );
+                  if (selection != null) {
+                    context.read<PreferencesProvider>().themeMode = selection;
+                  }
+                },
+              ),
+              const Divider(height: 1),
+              ListTile(
+                // used to vertically center the icon
+                leading: const SizedBox(
+                  height: double.infinity,
+                  child: Icon(Icons.line_weight),
+                ),
+                title: const Text('Schwierigkeitsgrad der Fragen'),
+                subtitle: Text(difficultyMap[difficulty] ?? 'Unbekannt'),
+                onTap: () async {
+                  final selection = await showDialog<DifficultyLevel>(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return SelectDialog(
+                        valueLabelMap: difficultyMap,
+                        value: context.select<PreferencesProvider, DifficultyLevel>((preferences) => preferences.difficulty),
+                        title: const Text('Schwierigkeitsgrad'),
+                      );
+                    }
+                  );
+                  if (selection != null) {
+                    context.read<PreferencesProvider>().difficulty = selection;
+                  }
+                },
               )
-      );
+            ],
+          ),
+        ),
+      )
+    );
   }
 }
