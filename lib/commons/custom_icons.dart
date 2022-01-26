c382d93bea29587fc592d265f1a8567b1869b2ce
René Apitzsch <rene.apitzsch@etit.tu-chemnitz.de>
2022-01-26 12:35:15 +0100

    Add custom Icons fitting public transport. Closes #39

--- lib/commons/custom_icons.dart
+++ lib/commons/custom_icons.dart
@@ -1,16 +1,18 @@
+import 'package:community_material_icon/community_material_icon.dart';
 import 'package:flutter/material.dart';
 
 const Map<String, IconData> customIcons = {
-  'shop': Icons.store,
-  'ticket': Icons.airplane_ticket,
-  'ticket_shop': Icons.airplane_ticket,
-  'bus': Icons.directions_bus,
-  'train': Icons.train,
-  'platform': Icons.home,
-  'stop': Icons.home,
-  'information': Icons.info,
-  'parking': Icons.local_parking,
-  'taxi': Icons.local_taxi,
-  'toilets': Icons.wc,
-  'phone': Icons.phone,
+  'stop': CommunityMaterialIcons.bus_stop,
+  'platform': CommunityMaterialIcons.bus_stop_covered,
+  'ticket_shop': CommunityMaterialIcons.ticket_confirmation,
+  'information': CommunityMaterialIcons.information_variant,
+  'ticket_machine': CommunityMaterialIcons.fridge_bottom,
+  'parking': CommunityMaterialIcons.parking,
+  'taxi': CommunityMaterialIcons.taxi,
+  'toilets': CommunityMaterialIcons.human_male_female,
+  'locker': CommunityMaterialIcons.locker_multiple,
+  'trolley': CommunityMaterialIcons.dolly,
+  'terminal_info': CommunityMaterialIcons.phone_message,
+  'terminal': CommunityMaterialIcons.phone_in_talk,
+  'terminal_emergency': CommunityMaterialIcons.phone_alert,
 };
