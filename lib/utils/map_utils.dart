2ebbff827b9e928adf4fe04584963e89f1b5bba5
Robbendebiene <robbendebiene@mailbox.org>
2022-02-14 08:41:57 +0100

    Improve rotation interpolation The interpolation will now always take the shortest direction which feels more natural. This is mainly noticeable when using the compass reset functionality. This code was taken from the animated location indicator.

--- lib/utils/map_utils.dart
+++ lib/utils/map_utils.dart
@@ -20,10 +20,9 @@ extension AnimationUtils on MapController {
     zoom ??= this.zoom;
     rotation ??= this.rotation;
 
-    final latTween = Tween<double>(begin: center.latitude, end: location.latitude);
-    final lngTween = Tween<double>(begin: center.longitude, end: location.longitude);
+    final positionTween = LatLngTween(begin: center, end: location);
     final zoomTween = Tween<double>(begin: this.zoom, end: zoom);
-    final rotationTween = Tween<double>(begin: this.rotation, end: rotation);
+    final rotationTween = RotationTween(begin: this.rotation, end: rotation);
 
     final controller = AnimationController(
       duration: duration,
@@ -36,7 +35,7 @@ extension AnimationUtils on MapController {
 
     animation.addListener(() {
       moveAndRotate(
-        LatLng(latTween.evaluate(animation), lngTween.evaluate(animation)),
+        positionTween.evaluate(animation),
         zoomTween.evaluate(animation),
         rotationTween.evaluate(animation),
         id: id
@@ -73,5 +72,47 @@ extension AnimationUtils on MapController {
       // floor so the bounds are always visible
       zoom: centerZoom.zoom.floorToDouble()
     );
+  }
+}
+
+
+/// Interpolate latitude and longitude values.
+
+class LatLngTween extends Tween<LatLng> {
+  static const piDoubled = pi * 2;
+
+  LatLngTween({ LatLng? begin, LatLng? end }) : super(begin: begin, end: end);
+
+  @override
+  LatLng lerp(double t) {
+    // latitude varies from [90, -90]
+    // longitude varies from [180, -180]
+
+    final latitudeDelta = end!.latitude - begin!.latitude;
+    final latitude = begin!.latitude + latitudeDelta * t;
+
+    // calculate longitude in range of [0 - 360]
+    final longitudeDelta = _wrapDegrees(end!.longitude - begin!.longitude);
+    var longitude = begin!.longitude + longitudeDelta * t;
+    // wrap back to [180, -180]
+    longitude = _wrapDegrees(longitude);
+
+    return LatLng(latitude, longitude);
+  }
+
+  double _wrapDegrees(v) => (v + 180) % 360 - 180;
+}
+
+
+/// Interpolate radians values in the direction of the shortest rotation delta / rotation angle.
+
+class RotationTween extends Tween<double> {
+  RotationTween({ double? begin, double? end }) : super(begin: begin, end: end);
+
+  @override
+  double lerp(double t) {
+    // thanks to https://stackoverflow.com/questions/2708476/rotation-interpolation
+    final rotationDelta = ((end! - begin!) + 180) % 360 - 180;
+    return begin! + rotationDelta * t;
   }
 }
