0a87704b54497ce86099b89f95a4946f3e087a9b
Robbendebiene <robbendebiene@mailbox.org>
2022-02-18 14:34:20 +0100

    Add release flag environment variable

--- lib/commons/osm_config.dart
+++ lib/commons/osm_config.dart
@@ -3,4 +3,6 @@ const appCallbackUrlScheme = 'openernext.de';
 const oAuth2ClientId = 'XWjiQpZ8E8eOq1i50M8UW68Z4HW_bwaeC3qj1nNLxNo';
 const oAuth2RedirectUri = '$appCallbackUrlScheme:/oauth2';
 
-const osmServerUri = 'https://master.apis.dev.openstreetmap.org';
+const osmServerUri = bool.fromEnvironment('IS_RELEASE', defaultValue: false)
+  ? 'https://www.openstreetmap.org'
+  : 'https://master.apis.dev.openstreetmap.org';
