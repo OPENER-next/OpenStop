0c230171bc4e47ccbc6b73cae8ebdd141fb21801
René Apitzsch <rene.apitzsch@etit.tu-chemnitz.de>
2022-02-21 13:28:47 +0100

    Fix validation check

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_map/flutter_map.dart';
