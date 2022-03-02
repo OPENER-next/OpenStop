a534b59facfa9ca02ca3d31c1d1db0b7a8adaa37
Robbendebiene <robbendebiene@mailbox.org>
2022-03-02 11:57:32 +0100

    Fix app start test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,27 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:nock/nock.dart';
+import 'package:opener_next/screens/home.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:opener_next/main.dart';
 
+const overpassReply = '''
+  {
+    "version": 0.6,
+    "generator": "",
+    "osm3s": {
+      "timestamp_osm_base": "",
+      "copyright": ""
+    },
+    "elements": []
+  }
+''';
+
 void main() {
+  setUpAll((){
+    nock.defaultBase = '';
+    nock.init();
+  });
+
+
+  setUp(nock.cleanAll);
+
+
   testWidgets('Basic app start test', (WidgetTester tester) async {
+    // mock preferences
     SharedPreferences.setMockInitialValues({
       'hasSeenOnboarding': false
     });
-    // Build our app and trigger a frame.
+    // mock overpass requests
+    nock.get(startsWith('https://overpass-api.de/api/interpreter'))
+      .reply(200, overpassReply, headers: {
+        'Content-Type': 'application/json'
+      });
+
+    nock.get(startsWith('https://overpass.kumi.systems/api/interpreter'))
+      .reply(200, overpassReply, headers: {
+        'Content-Type': 'application/json'
+      });
+
+    // build app and trigger first frame
     await tester.pumpWidget(MyApp(
       sharedPreferences: await SharedPreferences.getInstance()
     ));
 
-    final startButton = find.byType(ElevatedButton);
+    // skip through the onboarding screen
+    final nextButton = find.byType(OutlinedButton);
+    while(findsOneWidget.matches(nextButton, {})) {
+      await tester.tap(nextButton);
+      await tester.pumpAndSettle();
+    }
 
-    await tester.dragUntilVisible(
-      startButton,
-      find.byType(PageView),
-      const Offset(-250, 0),
-    );
-
-    expect(startButton, findsOneWidget);
+    expect(find.byType(HomeScreen), findsOneWidget);
   });
 }
