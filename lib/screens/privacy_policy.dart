513d00a1f680c88463407c1564a550b391ccae3c
Robbendebiene <robbendebiene@mailbox.org>
2022-04-21 13:32:37 +0200

    Improve privacy policy loading

--- lib/screens/privacy_policy.dart
+++ lib/screens/privacy_policy.dart
@@ -1,36 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class PrivacyPolicyScreen extends StatelessWidget {
+class PrivacyPolicyScreen extends StatefulWidget {
   const PrivacyPolicyScreen({Key? key}) : super(key: key);
 
-  void _launchUrl(String url) async {
-    if (!await launch(url)) throw '$url kann nicht aufgerufen werden';
+  @override
+  State<PrivacyPolicyScreen> createState() => _PrivacyPolicyScreenState();
+}
+
+class _PrivacyPolicyScreenState extends State<PrivacyPolicyScreen> {
+  final _privacyPolicyText = rootBundle.loadString('PRIVACY_POLICY.md');
+
+  void _launchUrl(String text, String? url, String? title) async {
+    if (url != null) {
+      if (!await launch(url)) throw '$url kann nicht aufgerufen werden';
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Datenschutzerklärung'),
-        ),
-        body: Scrollbar(
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.fromLTRB(40.0, 20.0, 40.0, 20.0),
-            scrollDirection: Axis.vertical,
-            child: FutureBuilder(
-                future: DefaultAssetBundle.of(context).loadString('PRIVACY_POLICY.md'),
-                builder: (context, snapshot) {
-                  return MarkdownBody(
-                      data: snapshot.data.toString(),
-                      selectable: true,
-                      onTapLink: (text, href, title) { if(href != null) _launchUrl(href); }
-                  );
-                }
-            )
-          ),
-        ),
+      appBar: AppBar(
+        title: const Text('Datenschutzerklärung'),
+      ),
+      body: Scrollbar(
+        child: FutureBuilder<String>(
+          future: _privacyPolicyText,
+          builder: (context, snapshot) {
+            return (!snapshot.hasData)
+              ? const Center(
+                child: CircularProgressIndicator()
+              )
+              : Markdown(
+                data: snapshot.requireData,
+                padding: EdgeInsets.only(
+                  top: 20,
+                  left: 20,
+                  right: 20,
+                  bottom: MediaQuery.of(context).padding.bottom
+                ),
+                selectable: true,
+                onTapLink: _launchUrl
+              );
+          }
+        )
+      ),
     );
   }
 }
