5d9a57eeba5ef8339dce73d9af1ba6bef89af40b
Robbendebiene <robbendebiene@mailbox.org>
2022-06-23 15:45:42 +0200

    Update nock package to resolve error and fix test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,17 +6,22 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:open_stop/main.dart';
 
-const overpassReply = '''
-  {
-    "version": 0.6,
-    "generator": "",
-    "osm3s": {
-      "timestamp_osm_base": "",
-      "copyright": ""
-    },
-    "elements": []
-  }
-''';
+const overpassReply = {
+  'version': 0.6,
+  'generator': '',
+  'osm3s': {
+    'timestamp_osm_base': '',
+    'copyright': ''
+  },
+  'elements': []
+};
+
+const osmPermissionReply =
+  '<?xml version="1.0" encoding="UTF-8"?>'
+  '<osm version="0.6" generator="OpenStreetMap server">'
+    '<permissions>'
+    '</permissions>'
+  '</osm>';
 
 void main() {
   setUpAll((){
@@ -34,28 +39,38 @@ void main() {
       'hasSeenOnboarding': false
     });
     // mock overpass requests
-    nock.get(startsWith('https://overpass-api.de/api/interpreter'))
-      .reply(200, overpassReply, headers: {
-        'Content-Type': 'application/json'
-      });
+    nock.get('https://overpass-api.de/api/interpreter')
+      ..query({'data': anything, 'bbox': anything})
+      ..reply(200, overpassReply, headers: { 'Content-Type': 'application/json' })
+      ..persist();
+    nock.get('https://overpass.kumi.systems/api/interpreter')
+      ..query({'data': anything, 'bbox': anything})
+      ..reply(200, overpassReply, headers: { 'Content-Type': 'application/json' })
+      ..persist();
+    // mock osm api requests
+    nock.get('https://master.apis.dev.openstreetmap.org/api/0.6/permissions')
+      ..reply(200, osmPermissionReply)
+      ..persist();
 
-    nock.get(startsWith('https://overpass.kumi.systems/api/interpreter'))
-      .reply(200, overpassReply, headers: {
-        'Content-Type': 'application/json'
-      });
+    // set screen size (mainly for emulator testing)
+    await tester.binding.setSurfaceSize(const Size(1080, 1920));
 
     // build app and trigger first frame
     await tester.pumpWidget(MyApp(
       sharedPreferences: await SharedPreferences.getInstance()
     ));
-
     // skip through the onboarding screen
-    final nextButton = find.byType(OutlinedButton);
-    while(findsOneWidget.matches(nextButton, {})) {
-      await tester.tap(nextButton);
-      await tester.pumpAndSettle();
-    }
+    final nextButton = find.byType(OutlinedButton, skipOffstage: true);
+    await tester.tap(nextButton);
+    await tester.pumpAndSettle();
+    await tester.tap(nextButton);
+    await tester.pumpAndSettle();
+    await tester.tap(nextButton);
+    await tester.pumpAndSettle();
+    await tester.tap(nextButton);
+    await tester.pump();
 
+    await tester.pump();
     expect(find.byType(HomeScreen), findsOneWidget);
   });
 }
