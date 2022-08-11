cbf88cfcd7a46dbc153e729dc7f038d007961c32
René Apitzsch <rene.apitzsch@etit.tu-chemnitz.de>
2022-08-11 15:44:32 +0200

    Unify duplicate functions

--- lib/models/map_feature.dart
+++ lib/models/map_feature.dart
@@ -2,16 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:community_material_icon/community_material_icon.dart';
 
 import '/models/osm_element_type.dart';
+import '/models/osm_matcher.dart';
 import '/commons/custom_icons.dart';
 
 
-class MapFeature {
+class MapFeature with OsmMatcher {
   final String name;
 
   final IconData icon;
 
+  @override
   final Map<String, dynamic> osmTags;
 
+  @override
   final List<OSMElementType> osmElements;
 
   const MapFeature({
@@ -42,44 +45,4 @@ class MapFeature {
   @override
   String toString() =>
       'MapFeature(name: $name, icon: $icon, osm_tags: $osmTags, osm_element: $osmElements)';
-
-  /// Check whether this map feature matches the given data.
-
-  bool matches(Map<String, String> tags, OSMElementType type) =>
-      matchesTags(tags) && matchesElement(type);
-
-
-  /// Check whether the tags of this map feature matches the given tags.
-
-  bool matchesTags(Map<String, String> tags) =>
-      osmTags.entries.every((entry) {
-        final value = tags[entry.key];
-        if (entry.value is Iterable) {
-          return entry.value.any(
-                  (condValue) => _conditionCheck(condValue, value)
-          );
-        }
-        else {
-          return _conditionCheck(entry.value, value);
-        }
-      });
-
-
-  /// Check whether the element types of this map feature matches the given element type.
-
-  bool matchesElement(OSMElementType type) =>
-      osmElements.isEmpty || osmElements.contains(type);
-
-
-  /// Returns true:
-  /// - if the condition value is true and the actual value is present (not null)
-  /// - if the condition value is false and the actual value is not present (is null)
-  /// - if the condition value is equal to the actual value
-
-  bool _conditionCheck (dynamic conditionValue, String? actualValue) {
-    if (conditionValue is bool) {
-      return (actualValue is String) == conditionValue;
-    }
-    return actualValue == conditionValue;
-  }
 }
--- lib/models/question_condition.dart
+++ lib/models/question_condition.dart
@@ -1,11 +1,15 @@
 import '/models/osm_element_type.dart';
+import '/models/osm_matcher.dart';
+
 
 /// This class holds the conditions that specify if a [Question] should be asked.
 
-class QuestionCondition {
+class QuestionCondition with OsmMatcher {
 
+  @override
   final Map<String, dynamic> osmTags;
 
+  @override
   final List<OSMElementType> osmElements;
 
   const QuestionCondition(this.osmTags, this.osmElements);
@@ -26,48 +30,6 @@ class QuestionCondition {
       osmElement
     );
   }
-
-
-  /// Check whether this condition matches the given data.
-
-  bool matches(Map<String, String> tags, OSMElementType type) =>
-    matchesTags(tags) && matchesElement(type);
-
-
-  /// Check whether the tags of this condition matches the given tags.
-
-  bool matchesTags(Map<String, String> tags) =>
-    osmTags.entries.every((tagCondition) {
-      final value = tags[tagCondition.key];
-      if (tagCondition.value is Iterable) {
-        return tagCondition.value.any(
-          (condValue) => _conditionCheck(condValue, value)
-        );
-      }
-      else {
-        return _conditionCheck(tagCondition.value, value);
-      }
-    });
-
-
-  /// Check whether the element types of this condition matches the given element type.
-
-  bool matchesElement(OSMElementType type) =>
-    osmElements.isEmpty || osmElements.contains(type);
-
-
-  /// Returns true:
-  /// - if the condition value is true and the actual value is present (not null)
-  /// - if the condition value is false and the actual value is not present (is null)
-  /// - if the condition value is equal to the actual value
-
-  bool _conditionCheck (dynamic conditionValue, String? actualValue) {
-    if (conditionValue is bool) {
-      return (actualValue is String) == conditionValue;
-    }
-    return actualValue == conditionValue;
-  }
-
 
   @override
   String toString() => 'QuestionCondition(osmTags: $osmTags, osmElements: $osmElements)';
