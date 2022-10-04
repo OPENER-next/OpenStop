42d2ad63cc002aeba36af28f930594c081839cfa
Robbendebiene <robbendebiene@mailbox.org>
2022-10-04 10:40:18 +0200

    Fix dispose exception and simplify code

--- lib/widgets/map_overlay/compass_button.dart
+++ lib/widgets/map_overlay/compass_button.dart
@@ -1,10 +1,11 @@
 import 'dart:math';
 import 'package:community_material_icon/community_material_icon.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 /// The rotation is expected in clockwise radians if not otherwise specified by the [isDegree] parameter.
-class CompassButton extends AnimatedWidget {
+class CompassButton extends StatelessWidget {
+  final double rotation;
+
   final void Function() onPressed;
 
   /// Whether the angle unit supplied by the [rotation] is in degrees or radians.
@@ -13,16 +14,14 @@ class CompassButton extends AnimatedWidget {
   static const _piFraction = pi / 180;
 
   const CompassButton({
-    required ValueListenable<double> rotation,
+    required this.rotation,
     required this.onPressed,
     this.isDegree = false,
     Key? key
-  }) : super(listenable: rotation, key: key);
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final rotation = (listenable as ValueListenable<double>).value;
-
     return FloatingActionButton.small(
       heroTag: null,
       onPressed: onPressed,
