14973b7ead28c7dba84247e9637964e8b13129b8
Robbendebiene <robbendebiene@mailbox.org>
2023-05-09 12:54:00 +0200

    Detach stop area layer from providers

--- lib/widgets/stops_layer/stop_area_layer.dart
+++ lib/widgets/stops_layer/stop_area_layer.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:provider/provider.dart';
 
-import '/utils/stream_utils.dart';
 import 'points_layer.dart';
 
 
@@ -15,18 +12,23 @@ class StopsLayer extends StatelessWidget {
 
   final Iterable<LatLng> incompleteStops;
 
+  /// The current zoom level.
+
+  final num currentZoom;
+
   /// The lowest zoom level on which the layer is still visible.
 
-  final int lowerZoomLimit;
+  final num lowerZoomLimit;
 
   /// The biggest zoom level on which the layer is still visible.
 
-  final int upperZoomLimit;
+  final num upperZoomLimit;
 
   const StopsLayer({
     required this.unloadedStops,
     required this.completedStops,
     required this.incompleteStops,
+    required this.currentZoom,
     this.lowerZoomLimit = 9,
     this.upperZoomLimit = 16,
     super.key,
@@ -35,43 +37,31 @@ class StopsLayer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final mapController = context.read<MapController>();
 
-    return StreamBuilder(
-      stream: mapController.mapEventStream
-        .whereType<MapEventWithMove>()
-        .map((event) => event.targetZoom.round())
-        .transform(ComparePreviousTransformer(
-          (previous, current) => previous != current
-        )),
-      builder: (context, AsyncSnapshot<int> snapshot) {
-        return AnimatedSwitcher(
-          duration: const Duration(milliseconds: 300),
-          child: !snapshot.hasData
-          || snapshot.requireData >= upperZoomLimit
-          || snapshot.requireData <= lowerZoomLimit
-            ? null
-            : Stack(
-              children: [
-                PointsLayer(
-                  points: unloadedStops,
-                  color: Colors.grey.shade400,
-                  radius: 3,
-                ),
-                PointsLayer(
-                  points: incompleteStops,
-                  color: theme.colorScheme.primary,
-                  radius: 3,
-                ),
-                PointsLayer(
-                  points: completedStops,
-                  color: const Color(0xFF8ccf73),
-                  radius: 3,
-                ),
-              ],
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 300),
+      child:
+        currentZoom >= upperZoomLimit || currentZoom <= lowerZoomLimit
+        ? null
+        : Stack(
+          children: [
+            PointsLayer(
+              points: unloadedStops,
+              color: Colors.grey.shade400,
+              radius: 3,
             ),
-        );
-      }
+            PointsLayer(
+              points: incompleteStops,
+              color: theme.colorScheme.primary,
+              radius: 3,
+            ),
+            PointsLayer(
+              points: completedStops,
+              color: const Color(0xFF8ccf73),
+              radius: 3,
+            ),
+          ],
+        ),
     );
   }
 }
