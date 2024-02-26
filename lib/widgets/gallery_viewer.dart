67dedebd86d945a8157f42790e9d71542eba7a01
Yulieth Ojeda <yulieth9109@gmail.com>
2024-02-26 09:55:51 +0100

    Forth Version

--- lib/widgets/gallery_viewer.dart
+++ lib/widgets/gallery_viewer.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import '/commons/themes.dart';
 import '/widgets/hero_viewer.dart';
@@ -68,50 +70,39 @@ class GalleryNavigator extends StatefulWidget {
   State<GalleryNavigator> createState() => _GalleryNavigatorState();
 }
 
-class _GalleryNavigatorState extends State<GalleryNavigator> with TickerProviderStateMixin {
-  bool _isZooming = false;
-  int _currentPageIndex = 0;
-  int _previousPageIndex = 0;
-  bool reverseLeft = false;
-  bool reverseRight = true;
+class _GalleryNavigatorState extends State<GalleryNavigator>{
+  bool _pagingEnabled = true;
   late final PageController _pageController;
-  late final AnimationController _leftArrow = AnimationController(
-    vsync: this, 
-    lowerBound : -1.0,
-    duration: const Duration(milliseconds: 300));
-  late final Animation<Offset> _leftAnimation = Tween<Offset>(
-    begin: const Offset(-1.0, 0.0),
-    end: Offset.zero)
-    .animate(_leftArrow);
-  late final AnimationController _rightArrow = AnimationController(
-    vsync: this, 
-    duration: const Duration(milliseconds: 300));
-  late final Animation<Offset> _rightAnimation = Tween<Offset>(
-    begin: Offset.zero,
-    end: const Offset(1.0, 0.0))
-    .animate(_rightArrow);
+  late final DerivedAnimation<PageController, double> _rightAnimation ;
+  late final DerivedAnimation<PageController, double> _leftAnimation;
 
   @override
   void initState() {
     super.initState();
     _pageController = PageController(initialPage: widget.initialIndex);
-    _pageController.addListener(_animateArrows);
-    if (widget.initialIndex == 0) {
-      _leftArrow.value = -1.0;
-      _leftArrow.reverse();
-    }
-    else {
-      _leftArrow.value = 0.0;
-      _leftArrow.forward();
-    }
-    if (widget.initialIndex < widget.images.length - 1 && widget.images.length > 1) {
-      _rightArrow.value = 0.0;
-      _rightArrow.reverse();
-    }
-    else {
-      _rightArrow.value = 1.0;
-      _rightArrow.forward();
-    }
+    _leftAnimation = DerivedAnimation<PageController, double>(notifier: _pageController, transformer: (controller) {
+      if (controller.hasClients && controller.page != null) {
+        return controller.page!.clamp(0,1);
+      } 
+      else if (controller.initialPage == 0) {
+        return 0.0;
+      }
+      else {
+        return 1.0;
+      }
+    });
+    _rightAnimation = DerivedAnimation<PageController, double>(notifier: _pageController, transformer: (controller) {
+      if (controller.hasClients && controller.page != null) {
+        final penultimate = widget.images.length - 2;
+        return (controller.page!.clamp(penultimate, widget.images.length -1) - penultimate).toDouble();
+      }
+      if (controller.initialPage < widget.images.length - 1 && widget.images.length > 1) {
+        return 0.0;
+      }
+      else {
+        return 1.0;
+      }
+    });
   }
 
   bool get hasPrevious {
@@ -138,69 +129,6 @@ class _GalleryNavigatorState extends State<GalleryNavigator> with TickerProvider
     }
   }
 
-  void _animateArrows() {
-    if (_pageController.hasClients && _pageController.page != null) {
-      _previousPageIndex = _currentPageIndex;
-      _currentPageIndex = _pageController.page!.round();
-      print('_previousPageIndex ' +  _previousPageIndex.toString() + '_currentPageIndex ' + _currentPageIndex.toString());
-      handleLeftArrow();
-      handleRightArrow();
-    }
-  }
-
-  void handleLeftArrow() {
-    double newValue = 0.0;
-    if (_pageController.page! > 0.0 && _pageController.page! < 1.0){
-      newValue = double.parse((-1.00 + _pageController.page!).toStringAsFixed(2));
-      if (newValue == -0.00) {
-        newValue = 0.0;
-      }
-      _leftArrow.reset();
-      _leftArrow.value = newValue;
-      if (_previousPageIndex > _currentPageIndex) {
-        reverseLeft = true;
-        _leftArrow.reverse();
-      }
-      else if (_previousPageIndex < _currentPageIndex){
-        reverseLeft = false;
-        _leftArrow.forward();
-      }
-      else {
-        if (reverseLeft == true) {
-          _leftArrow.reverse();
-        }
-        else {
-          _leftArrow.forward();
-        }
-      }
-    }
-  }
-
-  void handleRightArrow() {
-    double newValue = 0.0;
-    if (_pageController.page! >  widget.images.length - 2 && widget.images.length > 1){
-      newValue = double.parse((0.00 + (_pageController.page! - _pageController.page!.floor())).toStringAsFixed(2));
-      _rightArrow.reset();
-      _rightArrow.value = newValue;
-      if (_previousPageIndex > _currentPageIndex) {
-        reverseRight = true;
-        _rightArrow.reverse();
-      }
-      else if (_previousPageIndex < _currentPageIndex){
-        reverseRight = false;
-        _rightArrow.forward();
-      }
-      else {
-        if (reverseRight == true) {
-          _rightArrow.reverse();
-        }
-        else {
-          _rightArrow.forward();
-        }
-      }
-    }
-  }
-
   void goToPreviousImage() {
     _pageController.previousPage(
       duration: const Duration(milliseconds: 300),
@@ -221,96 +149,126 @@ class _GalleryNavigatorState extends State<GalleryNavigator> with TickerProvider
     super.dispose();
   }
 
-  void _handleScaleStart(ScaleStartDetails details) {
-    if (details.pointerCount == 2) {
-      _isZooming = true;
-    }
-    else {
-      _isZooming = false;
-    }
-    setState(() {});
-  }
-
-  void _handleScaleEnd(ScaleEndDetails details) {
-    setState(() {
-      _isZooming = false;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    final events = [];
     return Stack(
       alignment: Alignment.center,
       children: [
         Positioned.fill(
-          child: PageView.custom(
-            controller: _pageController,
-            scrollDirection: Axis.horizontal,
-            physics: _isZooming ? const NeverScrollableScrollPhysics() : null,   
-            childrenDelegate: SliverChildBuilderDelegate(
-              (context, index) {
-              return   InteractiveViewer(
-                onInteractionStart: _handleScaleStart,
-                onInteractionEnd: _handleScaleEnd,
-                maxScale: 3,
-                child: FittedBox(
-                fit: BoxFit.contain,
-                  child: Hero(
-                    tag:  widget.imagesKeys[index],
-                    child: Image.asset(
-                      widget.images[index],
-                      errorBuilder: (context, _, __) {
-                        return Image.asset(
-                          'assets/images/placeholder_image.png',
-                        );
-                      },
+          child: Listener(
+            behavior: HitTestBehavior.deferToChild,
+            onPointerDown: (event) {
+              events.add(event.pointer);
+              if (events.length >= 2) {
+                setState(() { _pagingEnabled = false; });
+              }
+            },
+            onPointerUp: (event) {
+              events.clear();
+              setState(() { _pagingEnabled = true; });
+            },
+            child: PageView.custom(
+              controller: _pageController,
+              scrollDirection: Axis.horizontal,
+              physics: _pagingEnabled ? const PageScrollPhysics() : const NeverScrollableScrollPhysics(),
+              childrenDelegate: SliverChildBuilderDelegate(
+                (context, index) {
+                return  InteractiveViewer(
+                  maxScale: 3,
+                  child: FittedBox(
+                  fit: BoxFit.contain,
+                    child: Hero(
+                      tag:  widget.imagesKeys[index],
+                      child: Image.asset(
+                        widget.images[index],
+                        errorBuilder: (context, _, __) {
+                          return Image.asset(
+                            'assets/images/placeholder_image.png',
+                          );
+                        },
+                      ),
                     ),
                   ),
-                ),
-              );
-            },
-            childCount: widget.images.length,
-            addAutomaticKeepAlives: false,
+                );
+              },
+              childCount: widget.images.length,
+              addAutomaticKeepAlives: false,
+              ),
             ),
           ),
         ),
         Positioned(
           left: 0,
           child: SafeArea(
-            child: AnimatedBuilder(
-              animation: _leftArrow,
-              builder: (BuildContext context, _) {
-                return SlideTransition(
-                  position: _leftAnimation,
-                  child:  IconButton(
-                    onPressed: hasPrevious ? goToPreviousImage : null,
-                    color: Theme.of(context).colorScheme.primary,
-                    icon: const Icon(Icons.arrow_circle_left, size: 30.0),
-                  ),
-                );
-              },
+            child: SlideTransition(
+              position: Tween<Offset>(
+                begin: const Offset(-1.0, 0.0),
+                end: Offset.zero,
+              ).animate(_leftAnimation),
+              child: IconButton(
+                onPressed: goToPreviousImage,
+                color: Theme.of(context).colorScheme.primary,
+                icon: const Icon(Icons.arrow_circle_left, size: 30.0),
+              ),
             ),
           ),
         ),
         Positioned(
           right: 0,
           child: SafeArea(
-            child: AnimatedBuilder(
-              animation: _rightArrow,
-              builder: (BuildContext context, _) {
-                return SlideTransition(
-                  position: _rightAnimation,
-                  child: IconButton(
-                    onPressed: hasNext ? goToNextImage : null,
-                    color: Theme.of(context).colorScheme.primary,
-                    icon: const Icon(Icons.arrow_circle_right, size: 30.0),
-                  ),
-                );
-              },
+            child: SlideTransition(
+            position: Tween<Offset>(
+                begin: Offset.zero,
+                end: const Offset(1.0, 0.0),
+              ).animate(_rightAnimation),
+              child: IconButton(
+                onPressed: goToNextImage,
+                color: Theme.of(context).colorScheme.primary,
+                icon: const Icon(Icons.arrow_circle_right, size: 30.0),
+              ),
             ),
           ),
         ),
       ],
     );
   }
+}
+
+class DerivedAnimation<T extends ChangeNotifier,V> extends Animation<V> {
+  final T _notifier;
+  final V Function(T) _transformer;
+
+  DerivedAnimation({
+    required T notifier,
+    required V Function(T) transformer,
+  }) : 
+  _notifier = notifier,
+  _transformer = transformer;
+
+  @override
+  void addListener(VoidCallback listener) {
+    _notifier.addListener(listener);
+  }
+
+  @override
+  void addStatusListener(AnimationStatusListener listener) {
+    // status will never change.
+  }
+
+  @override
+  void removeListener(VoidCallback listener) {
+    _notifier.removeListener(listener);
+  }
+
+  @override
+  void removeStatusListener(AnimationStatusListener listener) {
+    // status will never change.
+  }
+
+  @override
+  AnimationStatus get status => AnimationStatus.forward;
+
+  @override
+  V get value => _transformer.call(_notifier);
 }
