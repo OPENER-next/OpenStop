855502841ee8c9ed9e116fc27bfde02b143f5920
Robbendebiene <robbendebiene@mailbox.org>
2024-05-13 15:14:11 +0200

    Add general Overpass query API

---
+++ lib/api/overpass_query_api.dart
@@ -0,0 +1,136 @@
+import 'dart:math';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_map/flutter_map.dart';
+
+
+/// This class exposes API calls for making Overpass API requests.
+
+class OverpassQueryAPI {
+  final int maxRetries;
+
+  final Duration retryDelay;
+
+  final Duration sendTimeout;
+
+  final Duration receiveTimeout;
+
+  final List<String> apiServers;
+
+  final _dio = Dio();
+
+  final _random = Random();
+
+  OverpassQueryAPI({
+    this.maxRetries = 3,
+    this.retryDelay = const Duration(seconds: 1),
+    this.sendTimeout = const Duration(seconds: 10),
+    this.receiveTimeout = const Duration(seconds: 15),
+    this.apiServers = const [
+      'https://overpass.kumi.systems/api/interpreter',
+      'https://overpass-api.de/api/interpreter',
+    ],
+  });
+
+
+  /// Method to execute an Overpass query.
+  ///
+  /// If [bbox] is given the query will be limited to the given bbox.
+  /// See https://wiki.openstreetmap.org/wiki/Overpass_API/Overpass_QL#Global_bounding_box_(bbox)
+  ///
+  /// The query timeout can be set via the [timeout] parameter.
+  ///
+  /// This method will throw [Dio] connection errors.
+
+  Future<T> query<T>(
+    OverpassQuery<T> query, {
+    LatLngBounds? bbox,
+    Duration timeout = const Duration(seconds: 1),
+  }) async {
+    return query.responseTransformer(
+      await _query({
+        'data': query._fullQuery(
+          timeout: timeout,
+          globalBBox: bbox,
+        ),
+      }),
+    );
+  }
+
+  /// Actual query method that calls itself recursively by incrementing its retry counter till [maxRetries] is reached.
+
+  Future<Map<String, dynamic>?> _query(Map<String, dynamic> queryParameters, [ int retryCount = 1 ]) async {
+    // get random url from server list
+    final url = apiServers[_random.nextInt(apiServers.length)];
+
+    try {
+      return (await _dio.get<Map<String, dynamic>>(url,
+        queryParameters: queryParameters,
+        options: Options(
+          sendTimeout: sendTimeout,
+          receiveTimeout: receiveTimeout,
+        ),
+      )).data;
+    }
+    catch(error) {
+      if (retryCount < maxRetries) {
+        return Future.delayed(
+          retryDelay,
+          () => _query(queryParameters, retryCount + 1)
+        );
+      }
+      rethrow;
+    }
+  }
+
+  /// A method to terminate the api client and cleanup any open connections.
+  /// This should be called inside the widgets dispose callback.
+
+  void dispose() {
+    _dio.close(force: true);
+  }
+}
+
+
+/// Base class for Overpass queries.
+
+abstract class OverpassQuery<T> {
+
+  String _fullQuery({
+    required Duration timeout,
+    LatLngBounds? globalBBox,
+  }) {
+    return (
+      _queryHead(timeout: timeout, globalBBox: globalBBox)..write(query)
+    ).toString();
+  }
+
+  StringBuffer _queryHead({
+    required Duration timeout,
+    LatLngBounds? globalBBox,
+  }) {
+    final buffer = StringBuffer('[out:json]');
+
+    buffer..write('[timeout:')
+      ..write(timeout.inMilliseconds)
+    ..write(']');
+
+    if (globalBBox != null) {
+      buffer..write('[bbox:')
+        ..write(globalBBox.south)..write(',')
+        ..write(globalBBox.west) ..write(',')
+        ..write(globalBBox.north)..write(',')
+        ..write(globalBBox.east)
+      ..write(']');
+    }
+    return buffer..write(';');
+  }
+
+  /// The Overpass query body without header properties such as `[json]`.
+
+  String get query;
+
+  /// Callback to map the JSON response to an actual class or format it otherwise.
+
+  T responseTransformer(Map<String, dynamic>? response);
+}
