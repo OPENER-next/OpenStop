ff0366955d9fc8c6583b92a4901f44db8ca07974
René Apitzsch <rene.apitzsch@etit.tu-chemnitz.de>
2024-12-05 16:30:20 +0100

    Reset callbackUrlScheme for debug web builds

--- lib/commons/app_config.dart
+++ lib/commons/app_config.dart
@@ -8,6 +8,8 @@ const appVersion = packageVersion;
 
 const appUserAgent = '$appName $appVersion';
 
-const appCallbackUrlScheme = 'https';
+const appCallbackUrlScheme = kIsWeb && !bool.fromEnvironment('IS_RELEASE', defaultValue: false)
+    ? 'http'
+    : 'https';
 
 const appProjectUrl = 'https://github.com/OPENER-next/OpenStop';
