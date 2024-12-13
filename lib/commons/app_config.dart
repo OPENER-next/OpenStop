bfa1c34d5442a307833d1617f3d9006bba73eb79
René Apitzsch <18004390+7h30n3@users.noreply.github.com>
2024-12-13 11:26:09 +0100

    Enable App Links and Universal Links (#302)
    * Add intent filters for app links verification
    
    * Change ClientID and CallbackScheme
    
    * Switch to flutter_web_auth_2
    
    * Prepare for Universal Links on iOS
    
    * Enable Universal Links for iOS
    
    * Replace httpsHost and httpsPath with variables
    
    * Add documentation

--- lib/commons/app_config.dart
+++ lib/commons/app_config.dart
@@ -8,8 +8,12 @@ const appVersion = packageVersion;
 
 const appUserAgent = '$appName $appVersion';
 
-const appCallbackUrlScheme = kIsWeb
-  ? bool.fromEnvironment('IS_RELEASE', defaultValue: false) ? 'https' : 'http'
-  : 'openstop.app';
+const appCallbackUrlScheme = kIsWeb && !bool.fromEnvironment('IS_RELEASE', defaultValue: false)
+    ? 'http'
+    : 'https';
+
+const appCallbackUrlHost = 'openstop.app';
+
+const appCallbackUrlPath = '/oauth2';
 
 const appProjectUrl = 'https://github.com/OPENER-next/OpenStop';
--- lib/commons/osm_config.dart
+++ lib/commons/osm_config.dart
@@ -3,14 +3,14 @@ import 'package:flutter/foundation.dart';
 import 'app_config.dart';
 
 const oAuth2ClientId = bool.fromEnvironment('IS_RELEASE', defaultValue: false)
-  ? 'Y82_lwoOuvAiSVBGOwFehyYUkm-l9AEsh4BB-2ivL8U'
-  : 'dCjr74PY2yc2Ntk302zv0ENMTO9lnGFIR4o6nLnN3nE';
+  ? 'jkUcoZ_7SbO3zLsxtOubJeDe1_lUw-leKH5_mhRXDAE'
+  : 'F0hAZhRoqdSyQJyAGTRCeXVqwAw6RbSCS-2uAE0Fu8c';
 
 const oAuth2RedirectUri = kIsWeb
   ? bool.fromEnvironment('IS_RELEASE', defaultValue: false)
     ? '$appCallbackUrlScheme://try.openstop.app/oauth2.html'
     : '$appCallbackUrlScheme://127.0.0.1:8080/oauth2.html'
-  : '$appCallbackUrlScheme:/oauth2';
+  : '$appCallbackUrlScheme://$appCallbackUrlHost$appCallbackUrlPath';
 
 const osmServer = bool.fromEnvironment('IS_RELEASE', defaultValue: false)
   ? 'www.openstreetmap.org'
